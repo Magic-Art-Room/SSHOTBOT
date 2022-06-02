3bbcd89ecf67bb7004cef0318e8e96443cd06573
Magic-Art-Room <99250685+Magic-Art-Room@users.noreply.github.com>
2022-06-03 00:05:21 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -5,39 +5,16 @@ from bot.screenshotbot import ScreenShotBot
 from bot.config import Config
 
 
-BUTTONS = [[
-    InlineKeyboardButton('🏡 Home', callback_data='home'),
-    InlineKeyboardButton('🚸 Powered By', callback_data='t.me/MoviesFlixers_DL'),
-    InlineKeyboardButton('🔐 Close ', callback_data='close')
-]]
-
 HELP_TEXT = """
-Hey! {mention}. Welcome to Screenshot Generator Bot. You can use me to generate:
+**Usage Instructions**
 
-    1. Screenshots.
-    2. Sample Video.
-    3. Trim Video.
-
-☞ I support any kind of **telegram video file** (streaming video or document video files) provided it --has proper mime-type-- and --is not corrupted--.
-☞ I also support **Streaming URLs**. The URL should be a --streaming URL--, --non IP specific--, and --should return proper response codes--.
-☞ Just send me the telegram file or the streaming URL.
+Just Send me any File and after downloading complete it will show you some options like Screenshot or Trim or Sample and you have to choose one. 
 
 See /settings to configure bot's behavior.
 Use /set_watermark to set custom watermarks to your screenshots.
 
-**General FAQ.**
-
-☞ If the bot dosen't respond to telegram files you forward, first check /start and --confirm bot is alive--. Then make sure the file is a **video file** which satisfies above mentioned conditions.
-☞ If bot replies __😟 Sorry! I cannot open the file.__, the file might be --currupted-- or --is malformatted--.
-
-__If issues persists contact my father.__
-
-{admin_notification}
+**Bot Managed By @ARKBotz**
 """
-ADMIN_NOTIFICATION_TEXT = (
-    "Since you are one of the admins, you can check /admin to view the admin commands."
-)
-
 
 @ScreenShotBot.on_message(filters.private & filters.command("help"))
 async def help_(c, m):
