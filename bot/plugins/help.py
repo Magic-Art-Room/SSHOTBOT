a974b81ed502e8893c77fdffe748748510896fc5
Magic-Art-Room <99250685+Magic-Art-Room@users.noreply.github.com>
2022-06-03 00:30:00 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -21,6 +21,8 @@ ADMIN_NOTIFICATION_TEXT = (
     "Since you are one of the admins, you can check /admin to view the admin commands."
 )
 
+
+
 @ScreenShotBot.on_message(filters.private & filters.command("help"))
 async def help_(c, m):
 
